6dbac7e7991c8e1dce13109703e8b2c250237ef5
chaimaamiftah14 <chaimaamiftah13@gmail.com>
2024-10-29 14:16:50 +0100

    unit tests in js

---
+++ 0x06-unittests_in_js/test/example.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+describe('Sample Test Suite', function() {
+  it('should return true', function() {
+    assert.strictEqual(true, true);
+  });
+
+  it('should return false', function() {
+    assert.strictEqual(false, false);
+  });
+});
